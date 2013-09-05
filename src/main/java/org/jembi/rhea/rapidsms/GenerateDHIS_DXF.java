d2334f94a3a23d6cf0d6b46a04d43f3c7a998fab
ryan <rg.crichton@gmail.com>
2013-09-05 16:56:04 +0200

    Changed for using dataElements to events for DHIS

--- src/main/java/org/jembi/rhea/rapidsms/GenerateDHIS_DXF.java
+++ src/main/java/org/jembi/rhea/rapidsms/GenerateDHIS_DXF.java
@@ -5,28 +5,28 @@ import java.util.Date;
 
 public class GenerateDHIS_DXF {
 	
-	public static final String orgUnit = "testOrgUnit";
+	public static final String program = "xlMzncNto5k";
+	public static final String orgUnit = "nOHYeEmgwQs";
 	public static final String dataSetID = "dataSetID";
-	public static final String dataElementID = "dataElementID";
-	public static final String pregnancySuspected = "Suspected";
-	public static final String pregnancyConfirmed = "Confirmed";
+	public static final String pregnancySuspectedId = "YnxgptVUwPI";
+	public static final String pregnancyConfirmedId = "ZC64lHc3Khz";
 	
 	public static String generateDHIS_DXF(boolean confirmedPregnancy, Date date) {
 		
-		SimpleDateFormat sdfDate = new SimpleDateFormat("yyyyMMdd");
-		SimpleDateFormat sdfPeriod = new SimpleDateFormat("yyyyMM");
+		SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
 		String dateStr = sdfDate.format(date);
-		String periodStr = sdfPeriod.format(date);
 		
-		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" +
-					 "<dataValueSet xmlns=\"http://dhis2.org/schema/dxf/2.0\" dataSet=\"" + dataSetID + "\"" +
-					 	"completeDate=\""+ dateStr +"\" period=\"" + periodStr + "\" orgUnit=\"" + orgUnit + "\">";
+		String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+					 "<event program=\"" + program + "\" orgUnit=\"" + orgUnit  + "\" eventDate=\"" + dateStr + "\" completed=\"true\" storedBy=\"admin\">\n" +
+					 "	<coordinate latitude=\"59.8\" longitude=\"10.9\" />\n" +
+					 "	<dataValues>\n";
 		if (confirmedPregnancy) {
-			xml += "<dataValue dataElement=\"" + dataElementID + "\" value=\"" + pregnancyConfirmed + "\"/>";
+			xml += "		<dataValue dataElement=\"" + pregnancyConfirmedId + "\" value=\"1\"/>\n";
 		} else {
-			xml += "<dataValue dataElement=\"" + dataElementID + "\" value=\"" + pregnancySuspected + "\"/>";
+			xml += "		<dataValue dataElement=\"" + pregnancySuspectedId + "\" value=\"1\"/>\n";
 		}
-		xml += "</dataValueSet>";
+		xml += "	</dataValues>\n";
+		xml += "</event>";
 		
 		return xml;
 	}
